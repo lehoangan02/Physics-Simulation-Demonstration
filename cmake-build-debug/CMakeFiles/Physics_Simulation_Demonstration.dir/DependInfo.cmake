390064a9d96b195dbd4db2ced289a2e825c5f481
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-12-15 21:11:11 +0700

    Fix clion

---
+++ cmake-build-debug/CMakeFiles/Physics_Simulation_Demonstration.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Buttons/Buttons.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Buttons/Buttons.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Buttons/Buttons.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Commands/Command.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Commands/Command.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Commands/Command.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Machine Learning/KMeansCalculator.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Machine_Learning/KMeansCalculator.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Machine_Learning/KMeansCalculator.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Math/EquationsSolver.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Math/EquationsSolver.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Math/EquationsSolver.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Math/Geometry.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Math/Geometry.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Math/Geometry.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/Chains.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Chains.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Chains.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/Collider.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Collider.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Collider.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/Engine.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Engine.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Engine.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/PlatformRectangle.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/PlatformRectangle.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/PlatformRectangle.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/PlatformTriangle.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/PlatformTriangle.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/PlatformTriangle.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/RoundBall.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/RoundBall.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/RoundBall.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Physics/Spring.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Spring.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Physics/Spring.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/States/Observer.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/States/Observer.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/States/Observer.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/States/SimulationState.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/States/SimulationState.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/States/SimulationState.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Utilites/BackgroundMusic.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Utilites/BackgroundMusic.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Utilites/BackgroundMusic.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Utilites/Mediator.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Utilites/Mediator.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Utilites/Mediator.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/Utilites/Utilities.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/Utilites/Utilities.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/Utilites/Utilities.cpp.o.d"
+  "/home/lehoangan/Documents/GitHub/Physics-Simulation-Demonstration/main.cpp" "CMakeFiles/Physics_Simulation_Demonstration.dir/main.cpp.o" "gcc" "CMakeFiles/Physics_Simulation_Demonstration.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Physics_Simulation_Demonstration.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Physics_Simulation_Demonstration.
+# This may be replaced when dependencies are built.
